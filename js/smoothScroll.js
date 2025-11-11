3ba38aa35a0b0444e79a7152c981e8e735e6a0f7
Vika Ageeva <ageevavictory@gmail.com>
2025-11-11 14:36:03 +0700

    finish scroll-nav, remove not need files

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -3,6 +3,14 @@ const links = navbar.querySelectorAll('a')
 
 links.forEach((link) => {
     link.addEventListener('click', (event) => {
-        
+        event.preventDefault()
+        const section = document.querySelector(link.getAttribute('href'))
+        if (section) {
+            seamless.scrollIntoView(section, {
+                behavior: "smooth",
+                block: "start",
+                inline: "center",
+            });
+        }
     })
 })
